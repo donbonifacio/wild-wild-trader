420ba80af050ea813a17cda173231ae4d5c88f98
Pedro Santos <donbonifacio@gmail.com>
2015-09-05 15:40:42 +0100

    Fix node tests

--- src/wwtrader-playground/core.cljs
+++ src/wwtrader-playground/core.cljs
@@ -1,13 +1,19 @@
 (ns ^:figwheel-always wwtrader-playground.core
   (:require [reagent.core :as reagent]
             [secretary.core :as secretary]
-            #_[wwtrader-playground.views.layout :as layout]))
+            [wwtrader-playground.views.layout :as layout]))
 
 (enable-console-print!)
 
+(defn- browser?
+  "True if running on the browser"
+  []
+  js)
+
 (defn on-js-reload []
-  (println "Reloaded...")
-  (reagent/render-component [layout/render] (. js/document (getElementById "app"))))
+  (when (browser?)
+    (println "Reloaded...")
+    (reagent/render-component [layout/render] (. js/document (getElementById "app")))))
 
 (defn init []
   (secretary/set-config! :prefix "#")
--- src/wwtrader-playground/routes.cljs
+++ src/wwtrader-playground/routes.cljs
@@ -1,6 +1,7 @@
 (ns ^:figwheel-always wwtrader-playground.routes
   (:require [reagent.core :as reagent]
-            [secretary.core :as secretary]))
+            [secretary.core :as secretary]
+            [wwtrader-playground.state :as state]))
 
 (secretary/defroute "/game" []
   (state/set-page! :game))
